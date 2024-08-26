ea8898cc32d70b848843c397fd0578c5a0ca0b70
Eloi Alonso <ealonso@nvidia.com>
2024-08-26 12:58:42 +0200

    Double quote

--- iris_agent/agent.py
+++ iris_agent/agent.py
@@ -21,12 +21,12 @@ class Agent(nn.Module):
         super().__init__()
         name = name if name is not None else _prompt()
         sd = _load_ckpt(name)
-        sda = _extract_state_dict(sd, 'actor_critic')
-        sdt = _extract_state_dict(sd, 'tokenizer')
+        sda = _extract_state_dict(sd, "actor_critic")
+        sdt = _extract_state_dict(sd, "tokenizer")
         enc_dec_cfg = EncoderDecoderConfig(resolution=64, in_channels=3, z_channels=512, ch=64, ch_mult=(1, 1, 1, 1, 1),
                                            num_res_blocks=2, attn_resolutions=(8, 16), out_ch=3, dropout=0.0)
         self.tokenizer = Tokenizer(vocab_size=512, embed_dim=512, encoder=Encoder(enc_dec_cfg), decoder=Decoder(enc_dec_cfg))
-        self.actor_critic = ActorCritic(use_original_obs=False, act_vocab_size=sda['actor_linear.weight'].size(0))
+        self.actor_critic = ActorCritic(use_original_obs=False, act_vocab_size=sda["actor_linear.weight"].size(0))
         self.tokenizer.load_state_dict(sdt, strict=False)
         self.actor_critic.load_state_dict(sda)
 
@@ -49,26 +49,26 @@ class Agent(nn.Module):
 ######################
 
 
-GAMES = ['Alien', 'Amidar', 'Assault', 'Asterix', 'BankHeist', 'BattleZone', 'Boxing', 'Breakout', 'ChopperCommand', 'CrazyClimber', 'DemonAttack', 'Freeway', 'Frostbite', 'Gopher', 'Hero', 'Jamesbond', 'Kangaroo', 'Krull', 'KungFuMaster', 'MsPacman', 'Pong', 'PrivateEye', 'Qbert', 'RoadRunner', 'Seaquest', 'UpNDown']
+GAMES = ["Alien", "Amidar", "Assault", "Asterix", "BankHeist", "BattleZone", "Boxing", "Breakout", "ChopperCommand", "CrazyClimber", "DemonAttack", "Freeway", "Frostbite", "Gopher", "Hero", "Jamesbond", "Kangaroo", "Krull", "KungFuMaster", "MsPacman", "Pong", "PrivateEye", "Qbert", "RoadRunner", "Seaquest", "UpNDown"]
 
 
 def _extract_state_dict(state_dict, module_name):
-    return OrderedDict({k.split('.', 1)[1]: v for k, v in state_dict.items() if k.startswith(module_name)})
+    return OrderedDict({k.split(".", 1)[1]: v for k, v in state_dict.items() if k.startswith(module_name)})
 
 
 def _get_url_lfs(name):
-    first_url = f'https://api.github.com/repos/eloialonso/iris_pretrained_models/contents/pretrained_models/{name}.pt'
+    first_url = f"https://api.github.com/repos/eloialonso/iris_pretrained_models/contents/pretrained_models/{name}.pt"
     r = requests.get(first_url)
     assert r.status_code == 200
-    x = base64.b64decode(json.loads(r.content)['content']).decode()
-    sha = x.split('sha256:')[1].split('\n')[0]
-    size = int(x.split('size ')[1].split('\n')[0])
-    post_url = 'https://github.com/eloialonso/iris_pretrained_models.git/info/lfs/objects/batch'
+    x = base64.b64decode(json.loads(r.content)["content"]).decode()
+    sha = x.split("sha256:")[1].split("\n")[0]
+    size = int(x.split("size ")[1].split("\n")[0])
+    post_url = "https://github.com/eloialonso/iris_pretrained_models.git/info/lfs/objects/batch"
     post_json = json.dumps({"operation": "download", "transfers": ["basic"], "objects": [{"oid": sha, "size": size}]})
     post_headers = {"Accept": "application/vnd.git-lfs+json", "Content-type": "application/json"} 
     r = requests.post(post_url, post_json, headers=post_headers)
     assert r.status_code == 200
-    download_url = dict(json.loads(r.content.decode()))['objects'][0]['actions']['download']['href']
+    download_url = dict(json.loads(r.content.decode()))["objects"][0]["actions"]["download"]["href"]
     return download_url
 
 
@@ -76,28 +76,28 @@ def _load_ckpt(name):
     """Get checkpoint from cache or from git lfs server."""
     assert name in GAMES
     url = _get_url_lfs(name)
-    cache_dir = Path(f'{ROOTDIR}/checkpoints').absolute()
+    cache_dir = Path(f"{ROOTDIR}/checkpoints").absolute()
     cache_dir.mkdir(exist_ok=True, parents=False)
-    ckpt_path = cache_dir / f'{name}.pt'
+    ckpt_path = cache_dir / f"{name}.pt"
     if ckpt_path.is_file():
-        print(f'{name} checkpoint already downloaded at {ckpt_path}')
+        print(f"{name} checkpoint already downloaded at {ckpt_path}")
     else:
-        print(f'Downloading {name} checkpoint from https://github.com/eloialonso/iris_pretrained_models')
+        print(f"Downloading {name} checkpoint from https://github.com/eloialonso/iris_pretrained_models")
         r = requests.get(url, allow_redirects=True)
         if r.status_code != 200: 
-            raise ConnectionError('could not download {}\nerror code: {}'.format(url, r.status_code))
+            raise ConnectionError("could not download {}\nerror code: {}".format(url, r.status_code))
         ckpt_path.write_bytes(r.content)
-        print(f'Downloaded {name} checkpoint at {ckpt_path}')
-    return torch.load(ckpt_path, map_location='cpu')
+        print(f"Downloaded {name} checkpoint at {ckpt_path}")
+    return torch.load(ckpt_path, map_location="cpu")
 
 
 def _prompt():
-    games = '\n'.join([f'{i:2d}: {g}' for i, g in enumerate(GAMES)])
+    games = "\n".join([f"{i:2d}: {g}" for i, g in enumerate(GAMES)])
     print(games)
     while True:
-        id = input('\nEnter game id: ')
+        id = input("\nEnter game id: ")
         if id.isdigit() and 0 <= int(id) < len(GAMES): break
-        print(f'\n/!\ Invalid game id ({id}), please enter an integer between 0 and {len(GAMES) - 1}')
+        print(f"\n/!\ Invalid game id ({id}), please enter an integer between 0 and {len(GAMES) - 1}")
     name = GAMES[int(id)]
-    print(f'You chose {name}')
+    print(f"You chose {name}")
     return name
